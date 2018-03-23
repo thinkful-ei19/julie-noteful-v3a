8c8f9c1dae0a82d285e7b3520044f6feba043259
Julie K <julieskim160@gmail.com>
2018-03-23 08:23:55 -0700

    edit seed-database

--- routes/notes.js
+++ routes/notes.js
@@ -80,7 +80,7 @@ router.post('/notes', (req, res, next) => {
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/notes/:id', (req, res, next) => {
   const { id } = req.params;
-  const { title, content } = req.body;
+  const { title, content, folderId } = req.body;
 
   /***** Never trust users - validate input *****/
   if (!title) {
@@ -95,7 +95,7 @@ router.put('/notes/:id', (req, res, next) => {
     return next(err);
   }
 
-  const updateItem = { title, content };
+  const updateItem = { title, content, folderId };
   const options = { new: true };
 
   Note.findByIdAndUpdate(id, updateItem, options)
