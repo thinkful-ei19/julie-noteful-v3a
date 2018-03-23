0de1a21bdeef55250fc96373ba84d29bcd5634c7
Julie K <julieskim160@gmail.com>
2018-03-22 19:32:47 -0700

    describe get test

--- test/folders.test.js
+++ test/folders.test.js
@@ -32,7 +32,6 @@ describe('Noteful API - Folders', function () {
 
 
   describe('GET /api/folders', function() {
-
     it('should return the correct number of Folders and correct fields', function() {
       const dbPromise = Folder.find();
       const apiPromise = chai.request(app).get('/api/folders');
@@ -55,7 +54,7 @@ describe('Noteful API - Folders', function () {
   describe('GET /api/folder/:id', function() {
     it('should return the correct folder with given id', function() {
       let data;
-      return Folder.findOne().select('id', 'name')
+      return Folder.findOne().select('name')
         .then(_data => {
           data = _data;
           return chai.request(app).get(`/api/folders/${data.id}`);
@@ -72,7 +71,7 @@ describe('Noteful API - Folders', function () {
         });
     });
   
-    it.only('should respond with a 404 for an invalid id', function() {
+    it('should respond with a 404 for an invalid id', function() {
       return chai.request(app)
         .get('/api/folders/ALAKAZAMMOFOS')
         .catch(err => err.response)
@@ -80,6 +79,32 @@ describe('Noteful API - Folders', function () {
           expect(res).to.have.status(404);
         });
     });
+  });
+
+  describe('POST /api/folders', function() {
+    it('should create and return a new item when provided valid data', function() {
+      const newFolder = {
+        'name': 'What up'
+      };
+      let res;
+      return chai.request(app)
+        .post('/api/folders')
+        .send(newFolder)
+        .then(function (_res) {
+          res = _res;
+          expect(res).to.have.status(201);
+          expect(res).to.be.json;
+          expect(res).to.have.header('location');
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('id', 'name');
+          return Folder.findById(res.body.id);
+        })
+        .then(data => {
+          expect(res.body.name).to.equal(data.name);
+          expect(res.body.id).to.equal(data.id);
+        });
+    });
+    
   });
 
 });
